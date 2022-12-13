66fa62f61d46421548282c1600a49b7753544e20
GibadullinTagir <Gibadullin.t.r@gmail.com>
2022-12-13 19:32:08 +0300

    repair-img

--- script/index.js
+++ script/index.js
@@ -11,14 +11,6 @@ const swiper = new Swiper('.swiper', {
   let btn = document.querySelector(".catalog__btn-show-more");
   let btnImg = document.querySelector (".btn-show-more__img");
   
-  // let r;
-  // let xer;
-  // for( r = 6; r < a.length ; r++){
-  //   xer = a[r];
-  //   console.log(xer);
-  // };
-  // // console.log (xer);
-
   function elementHidden() {
     for(let i = 6; i < a.length ; i++){
       if ( a[i].style.display == "none" && document.documentElement.clientWidth < 1120 ) {
@@ -48,70 +40,6 @@ const swiper = new Swiper('.swiper', {
       } 
     }
   }
-
-
-  
-//     function elementHidden() {
-//       for(var i = 0; i < a.length ; i++){ 
-//         if ( a[i].style.display == "none" ) {
-//           a[i].style.display = "block";
-//           btnImg.style.rotate = "180deg";
-//           btn.textContent = "Скрыть";
-//         }
-//         else if (xer.style.display == "block" && document.documentElement.clientWidth < 1120 ) {
-//           xer.style.display = "none";
-//           btn.innerHTML = "Показать все";
-//           btnImg.style.rotate = "none";
-//         } 
-//         // // else  {
-//         //     a[i].style.display = "none";
-//         //     btn.innerHTML = "Показать все";
-//         //     btnImg.style.rotate = "none";
-//         //   // }
-//         }
-// };
-
-
-// function asd() {
-//   for(var i = 0; i < a.length ; i++){
-//     if ( a[i].style.display == "none" ) {
-//       a[i].style.display = "block";
-//       btnImg.style.rotate = "180deg";
-//       btn.textContent = "Скрыть";
-//     }
-//   }
-// }
-
-  
-//   function w768(){
-//     for(var i = 6; i < a.length ; i++){
-//       a[i].style.display = "block";
-//       btnImg.style.rotate = "180deg";
-//       btn.textContent = "Скрыть";
-//     }
-//   }
-  
-//   function w1120(){
-//     for(var i = 8; i < a.length ; i++){
-//       if (a[i].style.display == "block"){
-//       a[i].style.display = "none";
-//       btnImg.style.rotate = "180deg";
-//       btn.textContent = "Скрыть";
-//       }
-//     }
-//   }
-
-// && document.documentElement.clientWidth < 1119
-
-
-
-
-
-
-
-
-
-
 
 if(matchMedia) {
   let screen = window.matchMedia("(min-width:1120px)")
